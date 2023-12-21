79b24c965e7d7e5a6627281ca0613d62ba9eab6d
Gurveer Singh Virk <110109294+gurveervirk@users.noreply.github.com>
2023-12-21 23:01:57 +0530

    Update config.py
    reverted changes made (need to figure out why whatever happened as mentioned earlier happened)

--- module/florabert/config.py
+++ module/florabert/config.py
@@ -21,8 +21,8 @@ data_final = data / 'final'
 
 # Location of tools
 libs = root / 'libs'
-samtools = libs / 'samtools' / 'samtools'
-bedtools = libs / 'bedtools' / 'bedtools'
+samtools = libs / 'samtools'
+bedtools = libs / 'bedtools'
 dnabert = root / 'DNABERT'
 
 # Locations of specific files
