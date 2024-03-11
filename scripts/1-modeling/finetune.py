a40fb57171a74721db7c05174ca88c24d90d2703
André Ferreira <andrecnf@gmail.com>
2024-03-11 17:57:07 +0100

    fix: :bug: Typo in fine tuning script

--- scripts/1-modeling/finetune.py
+++ scripts/1-modeling/finetune.py
@@ -1,10 +1,13 @@
 """
 Fine-tuning the transformer model on the downstream gene expression prediction task
 """
+
 import sys
-sys.path.append('/kaggle/working/florabert')
+
+sys.path.append("/kaggle/working/florabert")
 import torch
 from torch.utils.data import DataLoader
+
 # import torch.distributed as dist
 from accelerate import Accelerator
 from tqdm.auto import tqdm
@@ -14,6 +17,7 @@ from module.florabert import transformers as tr
 from module.florabert.utils import compute_mse
 
 NUM_REPLICAS = 2
+
 
 def make_search_space(trial) -> dict:
     return {
@@ -40,6 +44,7 @@ def load_model(args, settings):
         **settings,
     )
 
+
 DATA_DIR = config.data_final / "transformer" / "genex" / "nam"
 TRAIN_DATA = "train.tsv"
 EVAL_DATA = "eval.tsv"
@@ -48,14 +53,13 @@ TOKENIZER_DIR = config.models / "byte-level-bpe-tokenizer"
 PREPROCESSOR = config.models / "preprocessor" / "preprocessor.pkl"
 
 # Starting from last checkpoint of the general purpose model
-PRETRAINED_MODEL = (
-    config.models / "transformer" / "language-model"
-)
+PRETRAINED_MODEL = config.models / "transformer" / "language-model"
 OUTPUT_DIR = config.models / "transformer" / "prediction-model"
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
+
 def _mp_fn():
-    accelerator = Accelerator(mixed_precision = 'fp16')
+    accelerator = Accelerator(mixed_precision="fp16")
     args = utils.get_args(
         data_dir=DATA_DIR,
         train_data=TRAIN_DATA,
@@ -73,7 +77,7 @@ def _mp_fn():
     )
     settings = utils.get_model_settings(config.settings, args)
 
-#     print(f"Model settings: {settings}")
+    #     print(f"Model settings: {settings}")
     print("Making model")
     config_obj, tokenizer, model = load_model(args, settings)
     if args.freeze_base:
@@ -104,8 +108,8 @@ def _mp_fn():
             nshards=args.nshards,
         )
 
-    dataset_train = datasets["train"].remove_columns(['sequence'])
-    dataset_test = datasets["eval"].remove_columns(['sequence'])
+    dataset_train = datasets["train"].remove_columns(["sequence"])
+    dataset_eval = datasets["eval"].remove_columns(["sequence"])
     print(f"Loaded training data with {len(dataset_train)} examples")
 
     if args.nshards_eval:
@@ -119,20 +123,20 @@ def _mp_fn():
     if args.num_train_epochs is not None:
         training_settings["num_train_epochs"] = args.num_train_epochs
     print(training_settings)
-    
+
     train_dataloader = DataLoader(
-        dataset_train, batch_size=64, collate_fn = data_collator, shuffle = True
+        dataset_train, batch_size=64, collate_fn=data_collator, shuffle=True
     )
     eval_dataloader = DataLoader(
-        dataset_test, batch_size=8, collate_fn = data_collator, shuffle = False
+        dataset_eval, batch_size=8, collate_fn=data_collator, shuffle=False
     )
-    
+
     model_init = lambda: load_model(args, settings)[2]  # For hyperparameter search
     optimizers = training.make_trainer(
         model,
         data_collator,
         dataset_train,
-        dataset_test,
+        dataset_eval,
         args.output_dir,
         hyperparameter_search=args.hyperparameter_search,
         model_init=model_init,
@@ -141,8 +145,10 @@ def _mp_fn():
     )
     progress_bar = tqdm(range(int(3 * len(train_dataloader) / NUM_REPLICAS)))
     optimizer, scheduler = optimizers
-    train_dataloader, eval_dataloader, model, optimizer, scheduler = accelerator.prepare(
-     train_dataloader, eval_dataloader, model, optimizer, scheduler
+    train_dataloader, eval_dataloader, model, optimizer, scheduler = (
+        accelerator.prepare(
+            train_dataloader, eval_dataloader, model, optimizer, scheduler
+        )
     )
     accelerator.print("Starting training")
     for epoch in range(3):
@@ -166,9 +172,9 @@ def _mp_fn():
             predictions = outputs.logits
             all_predictions.append(accelerator.gather(predictions))
             all_labels.append(accelerator.gather(batch["labels"]))
-            
-        all_predictions = torch.cat(all_predictions)[:len(datasets["eval"])]
-        all_labels = torch.cat(all_labels)[:len(datasets["eval"])]
+
+        all_predictions = torch.cat(all_predictions)[: len(datasets["eval"])]
+        all_labels = torch.cat(all_labels)[: len(datasets["eval"])]
 
         eval_metric = compute_mse(all_labels, all_predictions)
 
@@ -180,7 +186,7 @@ def _mp_fn():
             is_main_process=accelerator.is_main_process,
             save_function=accelerator.save,
         )
-        
+
     print("Saving model")
     unwrapped_model = accelerator.unwrap_model(model)
     unwrapped_model.save_pretrained(
